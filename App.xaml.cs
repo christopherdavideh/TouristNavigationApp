5b765d79d4b4db3644eb6e36fdb2c8f962de268c
danielalejandroquicaliquin@gmail.com <danielalejandroquicaliquin@gmail.com>
2024-05-05 15:09:01 -0500

    Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new Views.NavigationPage();
+            MainPage = new Views.DashboardPage();
         }
     }
 }
--- Views/DashboardPage.xaml.cs
+++ Views/DashboardPage.xaml.cs
@@ -23,6 +23,23 @@ public partial class DashboardPage : ContentPage
     }
 
 
+    private async void InicioButton_Tapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Mensaje", "INICIO", "OK");
+    }
+
+    private async void IAButton_Tapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Mensaje", "INTELIGENCIA ARTIFICIAL", "OK");
+    }
+
+    private async void PerfilButton_Tapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Mensaje", "PERFIL", "OK");
+    }
+
+
+
 
 
 }
