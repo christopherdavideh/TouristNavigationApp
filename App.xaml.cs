3c2a077889beb177ba3af3343277b1b3d5e850fb
christopherdavideh <chisk8_28@hotmail.com>
2024-05-18 23:47:45 -0500

    Add profile view

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TouristNavigationApp
+﻿using TouristNavigationApp.Views;
+
+namespace TouristNavigationApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new ProfilePage();
         }
     }
 }
---
+++ Views/ProfilePage.xaml.cs
@@ -0,0 +1,19 @@
+namespace TouristNavigationApp.Views;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+
+    private void editName_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void editEmail_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
