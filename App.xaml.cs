f7097014f57007cabdce9cc94e4c9bfdd8401884
christopherdavideh <chisk8_28@hotmail.com>
2024-05-23 04:28:00 -0500

    Load place on map

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,7 +7,13 @@ namespace TouristNavigationApp
         public App()
         {
             InitializeComponent();
-            MainPage = new NavigationPage(new MapsPage());
+            MainPage = new NavigationPage(new MapsPage())
+            {
+                
+                BarBackgroundColor = Colors.White,
+                BarTextColor = Colors.Black
+            };
+        
         }
     }
 }
---
+++ Models/Places.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristNavigationApp.Models
+{
+    class Places
+    {
+        public int plaId { get; set; }
+        public string plaName { get; set; }
+        public string plaDetail { get; set; }
+        public string plaReference { get; set; }
+        public double plaLatitude { get; set; }
+        public double plaLongitude { get; set; }
+        public string plaCity { get; set; }
+        public string plaProvince { get; set; }
+    }
+}
