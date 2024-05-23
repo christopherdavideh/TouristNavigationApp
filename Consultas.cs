18cde33b6296fbb051319dd094ae4fcb2ad962c5
“root” <rhidalgo@onepoint-corp.com>
2024-05-23 03:14:17 -0500

    ChatBot

---
+++ Consultas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouristNavigationApp
+{
+    public class Consultas
+    {
+        public string chatPregunta { get; set; } // Propiedad para la pregunta
+        public string chatRespuesta { get; set; } // Propiedad para la respuesta
+    }
+}
